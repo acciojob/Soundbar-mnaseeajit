42b779acf3592fb1956af207cc31be30785b69f0
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-21 17:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+var audio;
+
+function playSound(soundFileName) {
+    stopSound();  // Stop any currently playing sound
+
+    // Create an audio element
+    audio = new Audio('./sounds/' + soundFileName);
+
+    // Play the audio
+    audio.play();
+}
+
+function stopSound() {
+    if (audio) {
+        // Stop the audio if it's playing
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
